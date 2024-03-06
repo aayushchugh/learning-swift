e3dd28023e55dccc768846c60464291b548e3460
Ayush Chugh <ayushchugh2006@gmail.com>
2024-03-06 12:24:00 +0530

    Initial Commit

---
+++ Swift tutorial/Swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_tutorialApp.swift
+//  Swift tutorial
+//
+//  Created by Ayush Chugh on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
